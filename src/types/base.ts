a42a03faf30deb96706bf7bb5a90bc101b677004
CJL <2662442385@qq.com>
2024-04-16 16:51:00 +0800

    type: update

--- src/types/base.ts
+++ src/types/base.ts
@@ -2,4 +2,4 @@ export type TransferType = 'base64' | 'blob'
 
 export type BaseType = string | number
 
-export type Pixel = [number, number, number, number]
+export type Pixel = [R: number, G: number, B: number, A: number]
